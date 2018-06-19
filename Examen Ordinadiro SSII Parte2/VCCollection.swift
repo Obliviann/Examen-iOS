48a393db87de438b281fc0292c0f0f91867b56ae
OLIVIA SARTORIUS FRESCHET <olivia.sartorius@utmac0014.local>
2018-06-19 13:29:59 +0200

    Created VCCollection class (same steps as VCTabla)

---
+++ Examen Ordinadiro SSII Parte2/VCCollection.swift
@@ -0,0 +1,46 @@
+//
+//  VCCollection.swift
+//  Examen Ordinadiro SSII Parte2
+//
+//  Created by OLIVIA SARTORIUS FRESCHET on 19/6/18.
+//  Copyright © 2018 OLIVIA SARTORIUS FRESCHET. All rights reserved.
+//
+
+import UIKit
+
+class VCCollection: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet var colMiColleccion:UICollectionView?
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell : UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "miCelda2", for: indexPath)
+        return cell
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colMiColleccion?.delegate = self
+        colMiColleccion?.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
