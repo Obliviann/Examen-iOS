0aafb41c71aa134ffee1a56a6736387ee11a38da
OLIVIA SARTORIUS FRESCHET <olivia.sartorius@utmac0014.local>
2018-06-19 12:27:39 +0200

    Created VCRegistro class and connected it to View

---
+++ Examen Ordinadiro SSII Parte2/VCRegistro.swift
@@ -0,0 +1,35 @@
+//
+//  VCRegistro.swift
+//  Examen Ordinadiro SSII Parte2
+//
+//  Created by OLIVIA SARTORIUS FRESCHET on 19/6/18.
+//  Copyright © 2018 OLIVIA SARTORIUS FRESCHET. All rights reserved.
+//
+
+import UIKit
+
+class VCRegistro: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
