5edc2293932b377e4f35030160051a7a09068414
OLIVIA SARTORIUS FRESCHET <olivia.sartorius@utmac0014.local>
2018-06-19 11:30:55 +0200

    Created project and initialiced git rep

---
+++ Examen Ordinadiro SSII Parte2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Examen Ordinadiro SSII Parte2
+//
+//  Created by OLIVIA SARTORIUS FRESCHET on 19/6/18.
+//  Copyright © 2018 OLIVIA SARTORIUS FRESCHET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
