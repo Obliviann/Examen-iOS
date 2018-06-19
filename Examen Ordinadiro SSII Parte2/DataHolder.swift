62bbbc5d95dfc0ae9280306ef4cab90f3ce3bb74
OLIVIA SARTORIUS FRESCHET <olivia.sartorius@utmac0014.local>
2018-06-19 13:58:38 +0200

    Created DataHolder class

---
+++ Examen Ordinadiro SSII Parte2/DataHolder.swift
@@ -0,0 +1,13 @@
+//
+//  DataHolder.swift
+//  Examen Ordinadiro SSII Parte2
+//
+//  Created by OLIVIA SARTORIUS FRESCHET on 19/6/18.
+//  Copyright © 2018 OLIVIA SARTORIUS FRESCHET. All rights reserved.
+//
+
+import UIKit
+
+class DataHolder: NSObject {
+
+}
